5749da18b046e6fb0c105137ee4143a775b3619d
Reid <rlj0713@gmail.com>
2021-01-25 17:56:34 -0500

    commit tree 42688e9bfd1a7c56b6cc692c8bd5fb3ee7d5ae5d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+
+class Dog
+
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+       
+    end
+    
+    def self.all
+        @@all
+    end
+    
+    def self.clear_all
+        @@all = []     
+    end
+    
+    def self.print_all
+        i = 0
+        while i < @@all.length
+            puts @@all[i].name
+            i += 1
+        end
+    end
+    
+    def save
+        @@all << self
+    end
+    
+end
